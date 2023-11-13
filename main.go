3fd0752366592da68d677e1509228e42a6373afa
MCprotein <chss3339@gmail.com>
2023-11-13 23:38:36 +0900

    feat: switch

--- main.go
+++ main.go
@@ -127,3 +127,23 @@ func canIDrink(age int) bool {
 	return true
 
 }
+
+func canIDrinkSwitch(age int) bool {
+	switch koreanAge := age + 2; koreanAge {
+	case 10:
+		return false
+	case 18:
+		return true
+	}
+
+	switch {
+	case age < 18:
+		return false
+	case age == 18:
+		return true
+	case age > 50:
+		return false
+	}
+
+	return false
+}
