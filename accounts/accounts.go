986ad79043ef78411be43be3793d17c7ac6a842a
MCprotein <chss3339@gmail.com>
2023-11-16 22:21:24 +0900

    feat: method, error handling

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,5 +1,7 @@
 package accounts
 
+import "errors"
+
 /*
 Account struct
 struct는 생성자가 없다.
@@ -13,6 +15,8 @@ type Account struct {
 	balance int
 }
 
+var errNoMoney = errors.New("Can't withdraw you are poor")
+
 // NewAccount creates Account
 func NewAccount(owner string) *Account {
 	account := Account{owner: owner, balance: 0}
@@ -23,11 +27,23 @@ func NewAccount(owner string) *Account {
 Deposit x amount on your account
 function이 아니라 method이다.
 (account Account) 는 receiver라고 부른다.
+receiver의 type을 그대로 쓰면 Account의 복사본을 만들어서 사용한다.
+receiver의 type을 *Account로 쓰면 Account의 복사본을 만들지 않고 이 메소드를 호출한 account를 사용한다.
 */
-func (account Account) Deposit(amount int) {
+func (account *Account) Deposit(amount int) {
 	account.balance += amount
 }
 
 func (account Account) Balance() int {
 	return account.balance
+}
+
+// Withdraw x amount from your account
+func (account *Account) Withdraw(amount int) error {
+	if account.balance < amount {
+		return errNoMoney
+	}
+
+	account.balance -= amount
+	return nil
 }
--- main.go
+++ main.go
@@ -75,7 +75,14 @@ func main() {
 	structFunc()
 
 	account := accounts.NewAccount("nico")
+	fmt.Println(account)
 	account.Deposit(10)
+	fmt.Println(account.Balance())
+	err := account.Withdraw(20)
+	if err != nil {
+		// log.Fatalln(err)
+		fmt.Println(err)
+	}
 	fmt.Println(account.Balance())
 }
 
