0cc4cfa1ca8824a2a6148ebb291b3dfac7ca6650
MCprotein <chss3339@gmail.com>
2023-11-19 18:22:28 +0900

    feat: goroutine channel

--- goroutine/main.go
+++ goroutine/main.go
@@ -9,10 +9,26 @@ func main() {
 	/*
 		goroutine은 main function이 실행되는 동안에만 유효하다.
 		메인 함수는 goroutine을 기다려주지 않는다.
+
+		channel: gouroutine과 메인함수, 혹은 다른 goroutine 사이에 정보를 전달하기 위한 방법
+
 	*/
-	go sexyCount("nico")
-	go sexyCount("flynn")
+	channel := make(chan bool)
+	people := [2]string{"nico", "flynn"}
+	for _, person := range people {
+		go isSexy(person, channel)
+	}
+
+	result := <-channel
+	fmt.Println(result)
+
+	fmt.Println(<-channel)
+
+}
+
+func isSexy(person string, channel chan bool) {
 	time.Sleep(time.Second * 5)
+	channel <- true
 }
 
 func sexyCount(person string) {
