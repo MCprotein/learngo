ffb859c9acf3467aa5dff2d38d3e86a2ee7d0710
MCprotein <chss3339@gmail.com>
2023-11-20 22:39:32 +0900

    feat: goquery를 이용하여 페이지 가져오기 - 모든 정보 concat

--- job-scrapper/main.go
+++ job-scrapper/main.go
@@ -5,49 +5,57 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 type extractedJob struct {
-	id       string
-	location string
-	title    string
-	salary   string
-	career   string
+	id        string
+	location  string
+	title     string
+	salary    string
+	career    string
+	company   string
+	education string
 }
 
 // var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 var baseURL string = "https://www.saramin.co.kr/zf_user/search?search_area=main&search_done=y&search_optional_item=n&searchType=search&searchword=java"
 
 func main() {
+	jobs := []extractedJob{}
 	totalPages := getPages()
 	fmt.Println(totalPages)
 
 	for i := 1; i < totalPages; i++ {
-		pageUrl := baseURL + "&recruitPage=" + strconv.Itoa(i)
-		fmt.Println("Requesting ", pageUrl)
-		response, err := http.Get(pageUrl)
-		checkErr(err)
-		checkCode(response)
-
-		defer response.Body.Close()
-
-		doc, err := goquery.NewDocumentFromReader(response.Body)
-		checkErr(err)
-
-		searchCards := doc.Find(".item_recruit")
-
-		searchCards.Each(func(i int, card *goquery.Selection) {
-			// id, _ := card.Attr("value")
-
-			// title := card.Find(".area_job .job_tit>a").Text()
-
-			location := card.Find(".job_condition").Text()
-			fmt.Println(location)
-		})
+		extractedJobs := getPage(i)
+		jobs = append(jobs, extractedJobs...)
 
 	}
+	fmt.Println(jobs)
+}
+
+func getPage(page int) []extractedJob {
+	jobs := []extractedJob{}
+	pageUrl := baseURL + "&recruitPage=" + strconv.Itoa(page)
+	fmt.Println("Requesting ", pageUrl)
+	response, err := http.Get(pageUrl)
+	checkErr(err)
+	checkCode(response)
+
+	defer response.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(response.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".item_recruit")
+
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		job := extractJob(card)
+		jobs = append(jobs, job)
+	})
+	return jobs
 }
 
 func getPages() int {
@@ -83,5 +91,41 @@ func checkCode(response *http.Response) {
 }
 
 func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
+}
+
+func extractJob(card *goquery.Selection) extractedJob {
+	id, _ := card.Attr("value")
+
+	title := cleanString(card.Find(".area_job .job_tit>a").Text())
+
+	location := cleanString(card.Find(".job_condition>span>a").Text())
+
+	company := cleanString(card.Find(".area_corp .corp_name>a").Text())
+
+	career := ""
+	education := ""
+	salary := ""
+	card.Find(".job_condition>span").Each(func(i int, description *goquery.Selection) {
+		if i == 0 {
+			return
+		}
+
+		if i == 1 {
+			career = cleanString(description.Text())
+		}
+
+		switch i {
+		case 1:
+			career = cleanString(description.Text())
+		case 2:
+			education = cleanString(description.Text())
+		case 4:
+			salary = cleanString(description.Text())
+		}
+
+	})
+
+	return extractedJob{id: id, title: strings.TrimSpace(title), location: location, salary: salary, career: career, company: company, education: education}
 
 }
