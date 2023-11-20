4951d71a1876e61c87f2df1ee8b77f3f69d8d033
MCprotein <chss3339@gmail.com>
2023-11-20 21:51:26 +0900

    feat: goquery를 이용하여 페이지 가져오기 - id, title, location

--- job-scrapper/main.go
+++ job-scrapper/main.go
@@ -9,6 +9,14 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+type extractedJob struct {
+	id       string
+	location string
+	title    string
+	salary   string
+	career   string
+}
+
 // var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 var baseURL string = "https://www.saramin.co.kr/zf_user/search?search_area=main&search_done=y&search_optional_item=n&searchType=search&searchword=java"
 
@@ -19,6 +27,26 @@ func main() {
 	for i := 1; i < totalPages; i++ {
 		pageUrl := baseURL + "&recruitPage=" + strconv.Itoa(i)
 		fmt.Println("Requesting ", pageUrl)
+		response, err := http.Get(pageUrl)
+		checkErr(err)
+		checkCode(response)
+
+		defer response.Body.Close()
+
+		doc, err := goquery.NewDocumentFromReader(response.Body)
+		checkErr(err)
+
+		searchCards := doc.Find(".item_recruit")
+
+		searchCards.Each(func(i int, card *goquery.Selection) {
+			// id, _ := card.Attr("value")
+
+			// title := card.Find(".area_job .job_tit>a").Text()
+
+			location := card.Find(".job_condition").Text()
+			fmt.Println(location)
+		})
+
 	}
 }
 
@@ -52,4 +80,8 @@ func checkCode(response *http.Response) {
 	if response.StatusCode != 200 {
 		log.Fatalln("Request failed with Status: ", response.StatusCode)
 	}
+}
+
+func cleanString(str string) string {
+
 }
